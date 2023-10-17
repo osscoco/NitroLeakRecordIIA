a111220f653a4a53bb7a3f3bc5021f100aa3b07f
osscoco <corentin.jeannot2a@gmail.com>
2023-10-17 09:51:45 +0200

    Refactor by file include (section)

---
+++ resources/views/sections/artists.blade.php
@@ -0,0 +1,66 @@
+<!---Featured Artists Music--->
+<div class="ms_featured_slider" id="artistes">
+    <div class="ms_heading">
+        <h1>Artistes</h1>
+        <span class="veiw_all"><a href="#">voir plus</a></span>
+    </div>
+    <div class="ms_feature_slider swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="ms_rcnt_box">
+                    <div class="ms_rcnt_box_img">
+                        <img src="{{ asset('images/Artistes/corti.png') }}" width="252" alt="" class="img-fluid">
+                        <div class="ms_main_overlay">
+                            <div class="ms_box_overlay"></div>
+                            <div class="ms_more_icon">
+                                <img src="{{ asset('images/svg/more.svg') }}" alt="">
+                            </div>
+                            <ul class="more_option">
+                                <li><a href="#"><span class="opt_icon"><span class="icon icon_fav"></span></span>Favory</a></li>
+                                <li><a href="#"><span class="opt_icon"><span class="icon icon_queue"></span></span>File d'Attente</a></li>
+                                <li><a href="#"><span class="opt_icon"><span class="icon icon_dwn"></span></span>Télécharger</a></li>
+                                <li><a href="#"><span class="opt_icon"><span class="icon icon_playlst"></span></span>Playlist</a></li>
+                                <li><a href="#"><span class="opt_icon"><span class="icon icon_share"></span></span>Partager</a></li>
+                            </ul>
+                            <div class="ms_play_icon">
+                                <img src="{{ asset('images/svg/play.svg') }}" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ms_rcnt_box_text">
+                        <h3><a href="#">Corti</a></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="ms_rcnt_box">
+                    <div class="ms_rcnt_box_img">
+                        <img src="{{ asset('images/Artistes/kalu.png') }}" width="252" alt="">
+                        <div class="ms_main_overlay">
+                            <div class="ms_box_overlay"></div>
+                            <div class="ms_more_icon">
+                                <img src="{{ asset('images/svg/more.svg') }}" alt="">
+                            </div>
+                            <ul class="more_option">
+                                <li><a href="#"><span class="opt_icon"><span class="icon icon_fav"></span></span>Favory</a></li>
+                                <li><a href="#"><span class="opt_icon"><span class="icon icon_queue"></span></span>File d'Attente</a></li>
+                                <li><a href="#"><span class="opt_icon"><span class="icon icon_dwn"></span></span>Télécharger</a></li>
+                                <li><a href="#"><span class="opt_icon"><span class="icon icon_playlst"></span></span>Playlist</a></li>
+                                <li><a href="#"><span class="opt_icon"><span class="icon icon_share"></span></span>Partager</a></li>
+                            </ul>
+                            <div class="ms_play_icon">
+                                <img src="{{ asset('images/svg/play.svg') }}" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ms_rcnt_box_text">
+                        <h3><a href="#">Kalu</a></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Add Arrows -->
+    <div class="swiper-button-next1 slider_nav_next"></div>
+    <div class="swiper-button-prev1 slider_nav_prev"></div>
+</div>
---
+++ resources/views/sections/news.blade.php
@@ -0,0 +1,21 @@
+<!---Banner--->
+<div class="ms-banner" id="accueil">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <div class="ms_banner_img">
+                    <img src="{{ asset('images/Ce Mois-ci/nitro-corti.png') }}" width="406" alt="" class="img-fluid">
+                </div>
+                <div class="ms_banner_text">
+                    <h1>Ce mois-ci</h1>
+                    <h1 class="ms_color">Nouveaux Projets !</h1>
+                    <p>Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ea incidunt velit nihil ab consectetur modi tenetur quos ad, rerum aliquam quasi dolorum, sunt commodi facilis at doloribus totam? Delectus!<br> Things, The Heartbeat Stops, Walking Promises, Desired Games and many more...</p>
+                    <div class="ms_banner_btn">
+                        <a href="#" class="ms_btn">Ecouter</a>
+                        <a href="#" class="ms_btn">File d'attente</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
